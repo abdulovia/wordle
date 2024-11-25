f6d639e879dcd8f7397f715c85ee58fb07dfa28f
abdulovia <52665965+abdulovia@users.noreply.github.com>
2024-11-25 21:03:06 +0300

    add integration tests covering main wordle functionality

--- src/tests/test_difficulty_level.py
+++ src/tests/test_difficulty_level.py
@@ -6,9 +6,10 @@ from game.difficulty_level import DifficultyLevel
 class TestDifficultyLevelAndWordPicker(unittest.TestCase):
 
     def test_difficulty_level_and_word_picker(self):
-        level = DifficultyLevel(level="hard")
-        word_picker = WordPicker("resources/word_list.txt", level)
-        word_picker.load_words()
+        for level in ["easy", "medium", "hard"]:
+            difficulty_level = DifficultyLevel(level)
+            word_picker = WordPicker("resources/word_list.txt", difficulty_level)
+            word_picker.load_words()
 
-        secret_word = word_picker.pick_secret_word()
-        assert len(secret_word) == level.word_length
+            secret_word = word_picker.pick_secret_word()
+            assert len(secret_word) == difficulty_level.word_length
